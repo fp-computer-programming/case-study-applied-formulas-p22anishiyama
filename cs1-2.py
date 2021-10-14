eb3366c8f8a99ae17b4749b7801ae8762ef2c256
p22anishiyama <p22anishiyama@fairfieldprep.org>
2021-10-14 09:47:35 -0400

    add cs1-2.py

--- cs1-2.py
+++ cs1-2.py
@@ -1,9 +1,9 @@
 # Author: ATN 10/12/21
 
 principle_investment = float(input("Please enter initial deposit amount: "))
-annual_rate = int(input("Please enter the annual interest rate (as percentage): "))
+annual_rate = float(input("Please enter the annual interest rate (as a decimal): "))
 time = int(input("Please enter the number of years the money is in the account: "))
 number_compounded = 12
 
-future_value = principle_investment * (1 + (annual_rate / number_compounded) ** (number_compounded * time))
+future_value = principle_investment * (1 + (annual_rate / number_compounded)) ** (number_compounded * time)
 print("The value at this date is " + str(future_value))
